7ac2cd789ae44e48fc2a05f575ad1bc1ca2178d4
Alexia Mata <alexia.i.mata.perez@outlook.com>
2024-06-25 19:40:17 -0600

    Revision de vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portafolio/', // Asegúrate de que este sea el nombre de tu repositorio en GitHub
   plugins: [react()],
-})
+});
+
